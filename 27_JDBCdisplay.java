c817107b7331de5aa33b656c4b6f9c3faec58758
Bhushan Malviya <atharvamalviya02@gmail.com>
2025-11-20 20:03:00 +0530

    Added
    Implement a JDBC program to display records from a database table.

---
+++ 27_JDBCdisplay.java
@@ -0,0 +1,21 @@
+"Write a program that connects to a database using JDBC display all records in a table
+ import java.sql.*;
+  
+ public class DisplayRecords {
+  public static void main(String[] args) {
+  try {
+  Connection con = DriverManager.getConnection(
+  ""jdbc:mysql://localhost:3306/yourDB"", ""root"", ""password"");
+  Statement stmt = con.createStatement();
+  ResultSet rs = stmt.executeQuery(""SELECT * FROM your_table"");
+  
+  while (rs.next()) {
+  System.out.println(rs.getInt(1) + "" "" + rs.getString(2));
+  }
+  
+  con.close();
+  } catch (Exception e) {
+  System.out.println(e);
+  }
+  }
+ }"
